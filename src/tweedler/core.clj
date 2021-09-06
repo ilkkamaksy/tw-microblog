96189542bb665227ad869f9684a977291e792497
ilkkamaksy <ijmakine@gmail.com>
2021-09-06 14:40:40 +0300

    skeleton

--- src/tweedler/core.clj
+++ src/tweedler/core.clj
@@ -1,27 +1,58 @@
 (ns tweedler.core
-  (:require [ring.adapter.jetty :as jetty]
-            [ring.middleware.params :refer [wrap-params]]))
+  (:require [net.cgrand.enlive-html :as enlive]
+            [compojure.core :refer [defroutes GET POST]]
+            [compojure.route :refer [resources]]
+            [ring.adapter.jetty :as jetty]
+            [ring.middleware.params :refer [wrap-params]]
+            [markdown.core :refer [md-to-html-string]]
+            [clojure.string :refer [escape]]))
 
-(defn tweedler [request] 
-  (str "Hello " (get (:params request) "name")))
+(defrecord Tweed [title content])
 
-(defn response-middleware [handler]
-  (fn [request]
-    (let [response (handler request)]
-      (if (instance? String response)
-      {:body response
-       :status 200
-       :headers {"Content-Type" "text/html"}}
-      response
-        )
-      )
-    )
-  )
+(defprotocol TweedStore
+  (get-tweeds [store])
+  (put-tweed! [store tweed]))
 
-(def responseHandler
-  (-> tweedler
-      response-middleware
+(defrecord AtomStore [data])
+
+(extend-protocol TweedStore 
+  AtomStore
+  (get-tweeds [store]
+    (get @(:data store) :tweeds))
+  (put-tweed! [store tweed]
+    (swap! (:data store)
+           update-in [:tweeds] conj tweed)))
+
+(def store (->AtomStore (atom {:tweeds '()})))
+
+(enlive/defsnippet tweed-tpl "tweedler/index.html" [[:article.tweed enlive/first-of-type]]
+  [tweed]
+  [:.title] (enlive/html-content (:title tweed))
+  [:.content] (enlive/html-content (md-to-html-string (:content tweed))))
+
+(enlive/deftemplate index-tpl "tweedler/index.html"
+                    [tweeds]
+                    [:section.tweeds] (enlive/html-content (map tweed-tpl tweeds))
+                    [:form (enlive/set-attr :method "post" :action "/")])
+
+(defn escape-html [s]
+  (escape s {\> "&gt;" \< "&lt;"}))
+
+(defn handle-create [{{title "title" content "content"} :params}]
+  (put-tweed! store (->Tweed (escape-html title) (escape-html content)))
+  {:body "" :status 302 :headers {"Location" "/"}})
+
+(defroutes app-routes 
+  (GET "/" [] (index-tpl (get-tweeds store)))
+  (POST "/" req (handle-create req))
+  (resources "/css" {:root "tweedler/css"})
+  (resources "/img" {:root "tweedler/img"}))
+
+(def app 
+  (-> app-routes
       wrap-params))
 
-(defn -main []
-  (jetty/run-jetty responseHandler {:port 3000}))
+(def server (jetty/run-jetty #'app {:port 3000 :join? false}))
+
+;; (defn -main []
+;;   (jetty/run-jetty app {:port 3000}))
